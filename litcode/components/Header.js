d7e80ca0bc936d06e1145d66ef887c01c9f77909
mhedtman <mhedtman@student.42heilbronn.de>
2022-11-19 06:40:53 +0100

    href

--- litcode/components/Header.js
+++ litcode/components/Header.js
@@ -21,7 +21,7 @@ export default function Header() {
 					<Pic42 />
 				</button>
 			</div>
-				<a className="pr-20 href=https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-0cbc549c7a6a027ecae10d83f05c00faff575b7f725921ea8d7978aca6b4be32&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fcallback&response_type=code">
+				<a className="pr-20" href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-0cbc549c7a6a027ecae10d83f05c00faff575b7f725921ea8d7978aca6b4be32&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fcallback&response_type=code">
 					<BluePill />
 				</a>
 			</div>
