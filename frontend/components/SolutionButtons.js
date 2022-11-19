81d2d881d272ba3d3be00d1a51627d4425653b5f
mhedtman <mhedtman@student.42heilbronn.de>
2022-11-19 10:03:58 +0100

    clock

--- frontend/components/SolutionButtons.js
+++ frontend/components/SolutionButtons.js
@@ -9,7 +9,7 @@ export default function SolutionButtons() {
 			<button className="ml-10 text-base text-left bg-transparent hover:bg-green-500 text-green-700 font-mono hover:text-white py-0 px-2 border border-green-500 hover:border-transparent rounded">
 				Test
 			</button>
-			<div class = "bbg-transparent hover:bg-green-500 text-green-700 font-mono hover:text-white py-0 px-2 border border-green-500 hover:border-transparent rounded">
+			<div class = "ml-32 bbg-transparent hover:bg-green-500 text-green-700 font-mono hover:text-white py-0 px-2 border border-green-500 hover:border-transparent rounded">
 			<Timer />
 			</div>
 		</div>
